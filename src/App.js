04a45e7e86e25a5cedfe4d399cbce6fbacb4a972
Eira <91484524+Gamedev-Eira@users.noreply.github.com>
2025-07-18 10:43:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
 
   return ( 
 
-    <div> {
+    <div>
       
       <AppHeader SetNameValue = {SetNameValue} SetDateValue = {SetDateValue} SetUnitValue = {SetUnitValue} SetPriceValue = {SetPriceValue}/>
       
